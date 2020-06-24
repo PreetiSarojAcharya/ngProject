478375790acfcbcb0740d6df21e4fcd82e2f9ba7
PreetiSarojAcharya <Preeti.saroj@gmail.com>
2020-06-24 17:40:20 +0530

    PAckageRemoved

--- preeti-saroj/js/init.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-  $(function () {
-    $(".sidenav").sidenav();
-    $(".parallax").parallax();
-    $(".carousel").carousel();
-    $(".collapsible").collapsible();
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
